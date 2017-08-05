074d08745bd3032a3c73a1142876787c22831c90
reduanrafi <mr12129567@gmail.com>
2017-08-05 14:46:27 +0600

    app js file is modified

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const http = require('http');
 const hostname = '127.0.0.1';
-const fs = requr(fS)
+const fs = require('fs');
 const port = 3000;
-const server = http.createServer((req,res)=>{
+fs.readFile('index.html', (err, html)=>{
+	if(err){
+		throw err;
+	}
+	const server = http.createServer((req,res)=>{
    res.statusCode = 200;
-   res.setHeader('content-type','text/plain');
-   res.end('Hellow world');
+   res.setHeader('content-type','text/html');
+   res.write(html);
+   res.end();
 });
 
 server.listen(port,hostname,()=>{
   console.log("Server started at "+port);
+});
+
 });
